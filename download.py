076c207d4adfc834c62d1b5eb70f5b2d62314f8c
ComputerNetworkingHCMUS <118040121+ComputerNetworkingHCMUS@users.noreply.github.com>
2022-11-26 20:25:44 +0700

    Update download.py

--- download.py
+++ download.py
@@ -5,10 +5,7 @@ def downloadFile(input=" "):
     # print("Nhập url: ")
     # input=input()
     domain=input.split('//')[-1].strip('/').split('/')[0]
-    print(domain)
-
     subdirectory=input.split('//')[-1].lstrip(domain)
-    print(subdirectory)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((domain, 80))
     request = "GET "+subdirectory+" HTTP/1.1\r\nHost:"+domain+"\r\n\r\n"
@@ -24,7 +21,7 @@ def downloadFile(input=" "):
     print(content_length)
 
     filename = input.split('/')[-1]
-    data=data[data.find(b'\r\n\r\n'):-1]
+    data=data[data.find(b'\r\n\r\n')+4:]
     name_folder=subdirectory.split('/')[-2]
     dest_folder=domain+"_"+name_folder
     file_path = os.path.join(dest_folder, filename)
@@ -35,7 +32,7 @@ def downloadFile(input=" "):
     if not os.path.exists(dest_folder):
             os.makedirs(dest_folder) # create folder
             
-    while count <=content_length:
+    while count < content_length:
         with open(file_path, 'ab') as f:
             f.write(data)
             f.flush()
