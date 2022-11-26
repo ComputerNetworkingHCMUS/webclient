a6696bce9e5d7b7b7e0f6f00c833c1891b5f6639
ComputerNetworkingHCMUS <118040121+ComputerNetworkingHCMUS@users.noreply.github.com>
2022-11-26 21:13:41 +0700

    Update downloadSubfolder_edit.py

--- downloadSubfolder_edit.py
+++ downloadSubfolder_edit.py
@@ -23,28 +23,20 @@ if __name__ == "__main__":
     content_length=int(temp)
 
     name_folder=subdirectory.split('/')[-2]
-    #print(name_folder)
     con=subdirectory.replace(name_folder+"/","")
-    dest_folder=domain+"_"+name_folder
     
     data=data[data.find(b'\r\n\r\n')+4:]
     count=len(data)
-    if not os.path.exists(dest_folder):
-            os.makedirs(dest_folder)
     
     while count <=content_length:
         my_dict = re.findall('(?<=<a href=")[^"]*', str(data))
         for x in my_dict:
-        
             if x[0] == '#':
                 continue
             if x[0] == '/':
                 x = input + x
-           
             if (x != "?C=N;O=D") & (x != "?C=M;O=A") & (x != "?C=S;O=A") & (x != "?C=D;O=A") &(x!=input+con):
-                #download(input+x,dest_folder)
-                #print(x)
-                testDHT(input+x)
+                downloadFile(input+x)
         count=count+len(data)
         data = s.recv(1024*20)
     
