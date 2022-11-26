a09c980ab40471bfd1e5f40f129aacd0bc9b43cd
ComputerNetworkingHCMUS <118040121+ComputerNetworkingHCMUS@users.noreply.github.com>
2022-11-26 21:08:46 +0700

    Update downloadSubfolder_edit.py

--- downloadSubfolder_edit.py
+++ downloadSubfolder_edit.py
@@ -4,14 +4,15 @@ import re
 from download import downloadFile
 
 if __name__ == "__main__":
-    print("Nhập link: ")
+    print("Nhập address: ")
     input=input()
     domain=input.split('//')[-1].strip('/').split('/')[0]
     subdirectory=input.split('//')[-1].lstrip(domain)
     
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((domain, 80))
-    request = "GET "+subdirectory+" HTTP/1.1\r\nHost:"+domain+"\r\n\r\n"
+    request = "GET "+subdirectory+" HTTP/1.1\r\nHost:"+domain+"\r\nConnection: keep-alive\r\n\r\n"
     s.sendall(request.encode())
 
     data = s.recv(1000)
@@ -22,11 +23,14 @@ if __name__ == "__main__":
     content_length=int(temp)
 
     name_folder=subdirectory.split('/')[-2]
+    #print(name_folder)
     con=subdirectory.replace(name_folder+"/","")
+    dest_folder=domain+"_"+name_folder
     
-    data=data[data.find(b'\r\n\r\n'):-1]
+    data=data[data.find(b'\r\n\r\n')+4:]
     count=len(data)
-    
+    if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
     
     while count <=content_length:
         my_dict = re.findall('(?<=<a href=")[^"]*', str(data))
@@ -38,6 +42,8 @@ if __name__ == "__main__":
                 x = input + x
            
             if (x != "?C=N;O=D") & (x != "?C=M;O=A") & (x != "?C=S;O=A") & (x != "?C=D;O=A") &(x!=input+con):
+                #download(input+x,dest_folder)
+                #print(x)
                 testDHT(input+x)
         count=count+len(data)
         data = s.recv(1024*20)
