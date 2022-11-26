4ebf036a91023bc4b7a6d31acc26dc0b7f393795
ComputerNetworkingHCMUS <118040121+ComputerNetworkingHCMUS@users.noreply.github.com>
2022-11-26 21:07:45 +0700

    Update download.py

--- download.py
+++ download.py
@@ -2,15 +2,12 @@ import os
 import socket
 
 def downloadFile(input=" "):
-    # print("Nhập url: ")
-    # input=input()
     domain=input.split('//')[-1].strip('/').split('/')[0]
     subdirectory=input.split('//')[-1].lstrip(domain)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((domain, 80))
-    request = "GET "+subdirectory+" HTTP/1.1\r\nHost:"+domain+"\r\n\r\n"
+    request = "GET "+subdirectory+" HTTP/1.1\r\nHost:"+domain+"\r\nConnection: keep-alive\r\n\r\n"
     s.sendall(request.encode())
-
 
     data = s.recv(1000)
     header=data[0:data.find(b'\r\n\r\n')]
@@ -25,18 +22,22 @@ def downloadFile(input=" "):
     name_folder=subdirectory.split('/')[-2]
     dest_folder=domain+"_"+name_folder
     file_path = os.path.join(dest_folder, filename)
-    
     print("saving to", os.path.abspath(file_path))
     count=len(data)
     
+    
     if not os.path.exists(dest_folder):
             os.makedirs(dest_folder) # create folder
-            
-    while count < content_length:
-        with open(file_path, 'ab') as f:
+
+    with open(file_path, 'wb') as f:
+        f.write(data)
+        f.flush()
+        os.fsync(f.fileno())
+    with open(file_path, 'ab') as f:
+        while count<content_length:
+            data = s.recv(1000)
+            count=count+len(data)
             f.write(data)
             f.flush()
             os.fsync(f.fileno())
-        count=count+len(data)
-        data = s.recv(1000)
     s.close()
