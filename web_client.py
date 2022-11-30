71bd3fe6170f40aa3a5104cccec1ea6511d98cea
ComputerNetworkingHCMUS <118040121+ComputerNetworkingHCMUS@users.noreply.github.com>
2022-11-30 14:30:26 +0700

    Update web_client.py

--- web_client.py
+++ web_client.py
@@ -15,10 +15,11 @@ def threaded_function(input):
             # tạo socket và kết nối, nhận dữ liệu từ webserver
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((domain, 80))
-            request = "GET "+subdirectory+" HTTP/1.1\r\nHost:"+domain+"\r\n\r\n"
+            request = "GET /" + subdirectory + " HTTP/1.1\r\nHOST:" + \
+                domain + "\r\nConnection: keep-alive\r\n\r\n"
             s.sendall(request.encode())
             
-            #filename = input.split('/')[-1]
+            
             filename2=subdirectory.split('/')[-1]
             tempsub=subdirectory.split('/')
             
@@ -26,66 +27,100 @@ def threaded_function(input):
             header=data[0:data.find(b'\r\n\r\n')]
             temp=header.decode()
             
-            if (isValidDomain(str(filename2)) == False) and (len(tempsub)>2)  :
-                if temp.find('Content-Length')!=-1: # tìm thấy content-length
-                    temp=temp.split('Content-Length: ')[-1]
-                    temp=temp.split('\r\n')[0]
-                    content_length=int(temp)
+            
+            if isValidDomain(str(filename2)) == False and (len(tempsub) > 2):
+                    if temp.find('Content-Length')!=-1: # tìm thấy content-length
+                        temp=temp.split('Content-Length: ')[-1]
+                        temp=temp.split('\r\n')[0]
+                        content_length=int(temp)
 
-                    name_folder=subdirectory.split('/')[-2]
-                    con=subdirectory.replace(name_folder+"/","")
-                    
-                    data=data[data.find(b'\r\n\r\n'):-1]
-                    count=len(data)
-                    
-                    while count <=content_length:
-                        my_dict = re.findall('(?<=<a href=")[^"]*', str(data))
-                        for x in my_dict:
+                        name_folder=subdirectory.split('/')[-2]
+                        con=subdirectory.replace(name_folder+"/","")
                         
-                            if x[0] == '#':
-                                continue
-                            if x[0] == '/':
-                                x = input + x
+                        data=data[data.find(b'\r\n\r\n')+4:]
+                        count=len(data)
                         
-                            if (x != "?C=N;O=D") & (x != "?C=M;O=A") & (x != "?C=S;O=A") & (x != "?C=D;O=A") &(x!=input+con):
-                                downloadContentLength(input+x)
-                        count=count+len(data)
-                        data = s.recv(1024*20)
-                        
-                    print('Saving completed')
-                else:
-                    BUFF_SIZE = 1024*4
-                    timeout = 2
-                    s.setblocking(0)
-                    begin = time.time()
-                    while 1:
-                            # if you got some data, then break after wait sec
-                        if data and time.time()-begin > timeout:
-                            break
-                            # if you got no data at all, wait a little longer
-                        elif time.time()-begin > timeout*2:
-                            break
-                        try:
-                            data = s.recv(BUFF_SIZE)
-                            if data:
-                                my_dict = re.findall('(?<=<a href=")[^"]*', str(data))
+                        while count <=content_length:
+                            my_dict = re.findall('(?<=<a href=")[^"]*', str(data))
                             for x in my_dict:
-                                
+                            
                                 if x[0] == '#':
                                     continue
                                 if x[0] == '/':
                                     x = input + x
-                            
+                                
                                 if (x != "?C=N;O=D") & (x != "?C=M;O=A") & (x != "?C=S;O=A") & (x != "?C=D;O=A") &(x!=input+con):
-                                    download_chunked(input+x)
-                                begin = time.time()
+                                    downloadContentLength(input+x)
+                            count=count+len(data)
+                            data = s.recv(1024*20)
+                            
+                        print('Saving completed')
+
+                    else:
+                        file_name = ''
+                        if (subdirectory == ""):
+                            file_name = domain + "_index.html"
+                        else:
+                            reversed_file_dir = subdirectory[::-1]
+                            posfilename = reversed_file_dir.find("/")
+                            if (posfilename == -1):
+                                file_name = domain + "_" + subdirectory
                             else:
-                                time.sleep(0.1)
-                        except:
-                            pass
-                print('Saving completed')
+                                temp_file_name = reversed_file_dir[0:posfilename]
+                                temp_file_name = temp_file_name[::-1]
+                                file_name = file_name = domain + "_" + temp_file_name
+                        header = ""
+                        while not header.endswith('\r\n\r\n'):
+                            header += s.recv(1).decode()
+                        header = header[0:header.find('\r\n\r\n')]
+                        print(header)
+                        print('-------------------------------------------------------------------------------------------------')
+
+                        BUFFER_SIZE = 1024 * 2
+                        with open(file_name, 'wb') as f:
+                            while True:
+                                try:
+                                    # Get chunk size
+                                    chunkSize_line = ""
+                                    while not chunkSize_line.endswith('\n'):
+                                        chunkSize_line += s.recv(1).decode()
+                                    chunkSize_line = chunkSize_line[0:chunkSize_line.find('\r\n')]
+                                    chunk_size = int(chunkSize_line, 16)
+                                    print('Chunk_size = ', chunk_size)
+
+                                    # download and save chunked data
+                                    if chunk_size > 0:
+
+                                        n = chunk_size
+                                        while True:
+                                            if n >= BUFFER_SIZE:
+                                                data = s.recv(BUFFER_SIZE)
+                                            else:
+                                                data = s.recv(n+2)
+                                            #print(data)
+                                            my_dict = re.findall('(?<=<a href=")[^"]*', str(data))
+                                            for x in my_dict:
+                                            
+                                                if x[0] == '#':
+                                                    continue
+                                                if x[0] == '/':
+                                                    x = input + x
+                                                
+                                                if (x != "?C=N;O=D") & (x != "?C=M;O=A") & (x != "?C=S;O=A") & (x != "?C=D;O=A") &(x!=input+con):
+                                                    download_chunked(input+x)
+
+                                            n -= len(data)
+                                            if n + 2 == 0:
+                                                break
+                                        print('Saving...')
+                                except:
+                                    pass
+                                if chunk_size <= 0:
+                                    break
+                        print('Saving completed')
                     
             else:
+                # truong hop content-length
                 if temp.find('Content-Length')!=-1:
                     temp=temp.split('Content-Length: ')[-1]
                     temp=temp.split('\r\n')[0]
@@ -109,49 +144,84 @@ def threaded_function(input):
                             count=count+len(data)
                             f.write(data)
                             f.flush()
-                else:
-                    filename = input.split('/')[-1]
-                    if filename=="":
-                        filename=domain+"_"+"index.html"
-                    else :
-                        filename=domain+"_"+filename
-                    print("saving to", os.path.abspath(filename))
+                            
+                    print('Saving completed')
                     
-                    data = data[data.find(b'\r\n\r\n')+4:]
-                    with open(os.path.abspath(filename), 'wb') as f:
-                        f.write(data)
-                        f.flush()
+                else:
+                    # trường hợp chunked
+                    if (input.find("http://") != -1):
+                        input = input.replace("http://", '')
+                    poshost = Input.find("/")
+                    if (poshost == -1):
+                        domain = input.split('//')[-1].strip('/').split('/')[0]
+                        subdirectory = ""
+                    else:
+                        domain = input.split('//')[-1].strip('/').split('/')[0]
+                        subdirectory = input.split('//')[-1].lstrip(domain)
 
-                    BUFF_SIZE = 1024*4
-                    timeout = 2
-                    s.setblocking(0)
-                    begin = time.time()
-                    with open(os.path.abspath(filename), 'ab') as f:
-                        while 1:
-                            # if you got some data, then break after wait sec
-                            if data and time.time()-begin > timeout:
-                                break
-                            # if you got no data at all, wait a little longer
-                            elif time.time()-begin > timeout*2:
-                                break
+                    file_name = ''
+                    if (subdirectory == ""):
+                        file_name = domain + "_index.html"
+                    else:
+                        reversed_file_dir = subdirectory[::-1]
+                        posfilename = reversed_file_dir.find("/")
+                        if (posfilename == -1):
+                            file_name = domain + "_" + subdirectory
+                        else:
+                            temp_file_name = reversed_file_dir[0:posfilename]
+                            temp_file_name = temp_file_name[::-1]
+                            file_name = file_name = domain + "_" + temp_file_name
+                    header = ""
+                    while not header.endswith('\r\n\r\n'):
+                        header += s.recv(1).decode()
+                    header = header[0:header.find('\r\n\r\n')]
+                    print(header)
+                    print('-------------------------------------------------------------------------------------------------')
+
+                    BUFFER_SIZE = 1024 * 2
+                    with open(file_name, 'wb') as f:
+                        while True:
                             try:
-                                data = s.recv(BUFF_SIZE)
-                                if data:
-                                    f.write(data)
-                                    f.flush()
-                                    begin = time.time()
-                                else:
-                                    time.sleep(0.1)
+                                # Get chunk size
+                                chunkSize_line = ""
+                                while not chunkSize_line.endswith('\n'):
+                                    chunkSize_line += s.recv(1).decode()
+                                chunkSize_line = chunkSize_line[0:chunkSize_line.find('\r\n')]
+                                chunk_size = int(chunkSize_line, 16)
+                                print('Chunk_size = ', chunk_size)
+
+                                # download and save chunked data
+                                if chunk_size > 0:
+
+                                    n = chunk_size
+                                    while True:
+                                        if n >= BUFFER_SIZE:
+                                            data = s.recv(BUFFER_SIZE)
+                                        else:
+                                            data = s.recv(n+2)
+                                        #print(data)
+                                        f.write(data)
+                                        f.flush()
+
+                                        n -= len(data)
+                                        if n + 2 == 0:
+                                            break
+                                    print('Saving...')
                             except:
                                 pass
-
+                            if chunk_size <= 0:
+                                break
                     print('Saving completed')
+                    s.close()
+                    
             time.sleep(0.1)
         except:
             time.sleep(0.1)
             print('Ngắt kết nối')
             s.close()
 
+       
+        
 
 def isValidDomain(str):
     regex = "^((?!-)[A-Za-z0-9-]" + "{1,63}(?<!-)\\.)" + "+[A-Za-z]{2,6}"
@@ -166,7 +236,7 @@ def isValidDomain(str):
  
     # Return if the string
     # matched the ReGex
-    if((re.search(p, str)) or (re.findall('.ppt',str)) or (re.findall(".docx",str))):
+    if((re.search(p, str)) or (re.findall('.ppt',str)) or (re.findall(".docx",str)) ):
         return True
     else:
         return False
@@ -175,9 +245,6 @@ def isValidDomain(str):
 if __name__ == "__main__":
     print('Nhập link(s): ')
     Input=input()
-    count=1
-    if(re.findall('http',Input)):
-        count=count+1
     tempin=Input.split(" ")
     
     if len(tempin) > 1:
@@ -197,9 +264,6 @@ if __name__ == "__main__":
     while choose == 1:
         print('Nhập link(s): ')
         Input=input()
-        count=1
-        if(re.findall('http',Input)):
-            count=count+1
         tempin=Input.split(" ")
         
         if len(tempin) > 1:
